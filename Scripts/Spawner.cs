3901919c09f6f4816c8a3e914f2d2d10d4ad71dc
Dankisey <greeng3n@gmail.com>
2021-11-06 15:56:33 +0300

    -

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoints;
+    [SerializeField] private GameObject _enemyToCreate;
+    [SerializeField] private int _amount;
+    [SerializeField] private int _radius;
+
+    private Transform[] _spawners;
+
+    private void Start()
+    {
+        int angleStep = 360 / _amount;
+
+        _spawners = new Transform[_spawnPoints.childCount];
+
+        for (int i = 0; i < _spawnPoints.childCount; i++)
+        {
+            _spawners[i] = _spawnPoints.GetChild(i);
+        }
+
+        StartCoroutine(SpawnEnemies(angleStep));
+    }
+
+    private IEnumerator SpawnEnemies(int angleStep)
+    {
+        var waitForTwoSeconds = new WaitForSeconds(2);
+
+        for (int currentSpawner = 0; currentSpawner < _spawners.Length; currentSpawner++)
+        {
+            for (int i = 0; i < _amount; i++)
+            {
+                GameObject newEnemy = Instantiate(_enemyToCreate, _spawners[currentSpawner].position, Quaternion.identity);
+
+                newEnemy.transform.position += new Vector3(_radius * Mathf.Cos(angleStep * (i + 1) * Mathf.Deg2Rad), _radius * Mathf.Sin(angleStep * (i + 1) * Mathf.Deg2Rad), 0);
+            }
+
+            yield return waitForTwoSeconds;
+        }  
+    }
+}
